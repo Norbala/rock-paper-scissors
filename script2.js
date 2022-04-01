bdd899f3fb69694786abd678cc4e95d4c2de268a
norbala <lobyous.@gmail.com>
2022-04-01 23:47:51 +0200

    gameplay, score corrected

--- script2.js
+++ script2.js
@@ -34,15 +34,6 @@ buttons.forEach((button) => {
 }); 
 
 
-/* modal_content.addEventListener("click", () => {
-    modal.style.display = "block";
-});
-*/
-
-/* modal.addEventListener("click", () => {
-    close_modal();
-}); */
-
 window.addEventListener("click", (event) => {       // jebote, s windowom se da rijesiti
     if (event.target==modal) {
         modal.style.display = "none";
@@ -70,10 +61,12 @@ function computerPlay () {
 
 function singleGame() {             // tutorial je razdvojio userinput u jednu funkciju i if-scoring u drugu funkciju, a u mene je to zajedno
     console.log("user score ", userScore);
-    if (userScore==5 || compScore==5) {
+    if (userScore===5) {
         modal.style.display = "block";
     }
-
+    else if (compScore===5) {
+        modal.style.display = "block";
+    }
     else if ((userinput==="Rock"&&compRes==="Rock")||(userinput==="Scissors"&&compRes==="Scissors")||(userinput==="Paper"&&compRes==="Paper")) {
         console.log("It's a tie!");
         flavour.textContent = '"All guys are same" (Tie)';
@@ -83,13 +76,13 @@ function singleGame() {             // tutorial je razdvojio userinput u jednu f
         compScore++;
         flavour.textContent = 'You lost this one! He screwed you... and her';
     }
-    else if ((userinput==="Rock"&&compRes==="Scissors")||(userinput==="Scissors"&&compRes==="Paper")||(userinput==="Paper"&&compRes==="Rock")) {
+    else /* ((userinput==="Rock"&&compRes==="Scissors")||(userinput==="Scissors"&&compRes==="Paper")||(userinput==="Paper"&&compRes==="Rock")) */ {
         console.log("You won!");
         userScore++;
         flavour.textContent = 'You win! There is still some hope for you... Cope*';
-    }
+    };  // if 
 };
-
+                    // IMAM bug kad je score 4:5. umjesto da pokaze win za PC, odigra se jos i iduci put pobijedim
 function Game() {
     
     compRes = computerPlay();
@@ -102,26 +95,28 @@ function Game() {
     
     if (compScore===5 && compScore_check===1) {
         modal.style.display = "block";
-        modal_text.textContent = "He is gonna cum and you gonna swalla! BUAHAHAH GAME OVER FO YOU";       // izbaci ovo
+        modal_text.textContent = "He is gonna cum and you gonna swalla! BUAHAHAH GAME OVER FO YOU";
+        flavour2.textContent = "";
         
-    }
+    };
     
-    else if (userScore===5 && userScore_check===1) {
+    if (userScore===5 && userScore_check===1) {
         modal.style.display = "block";
         modal_text.textContent = "You rocked that fucker! You'll still bang that hoe... for now!";
         // isto ko gore
+        flavour2.textContent = "";
+    };
 
-    }
-    else if (compScore===4 && userScore===4) {
+    if (compScore===4 && compScore_check===0) {
+        compScore_check = 1;
+    };
+    if (userScore===4 && userScore_check===0) {       
+        userScore_check = 1;
+    };
+    if (compScore===4 && userScore===4) {
         flavour2.textContent = "It's a really tough one!! You two fuckers slayin each other for d hoe";
         console.log("It's a really tough one!!!!\nHold on!");
-    }
-    else if (compScore===4 && compScore_check===0) {
-            compScore_check =1;
-    }
-    else if (userScore===4 && userScore_check===0) {       
-        userScore_check = 1;
-    };  
+    };
 };
 
 function reset_game () {
@@ -136,6 +131,3 @@ function reset_game () {
         flavour2.textContent = "";
 };
 
-function close_modal () {
-    modal.style.display = "none";
-};  
